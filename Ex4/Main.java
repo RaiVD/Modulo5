4926bed39b66b23db43b84a5c8318ee6c1b97058
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-05-31 11:22:17 -0300

    Exercicio

--- Ex4/Main.java
+++ Ex4/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         List<Conta> contas = new ArrayList<>();
 
-        Conta contaRaissa = new Conta(0,0,0);
-        Conta contaAna = new Conta(0,0,0);
-        Conta contaPedro = new Conta(0,0,0);
-        Conta contaPaulo = new Conta(0,0,0);
+        Conta contaRaissa = new Conta(1550,2341,2234);
+        Conta contaAna = new Conta(1000,1467,2134);
+        Conta contaPedro = new Conta(2000,1234,2222);
+        Conta contaPaulo = new Conta(3500,1425,2143);
 
         contas.add(contaRaissa);
         contas.add(contaAna);
@@ -18,8 +18,10 @@ public class Main {
 
 
         for (Conta calculo :contas) {
-            System.out.println("Poupança com tributo: "+calculo.tributosPoupanca()+"\nCorrente com tributo:  "+calculo.tributosContaCorrente()+"\nSeguro de vida com tributo: "+calculo.tributosSeguro());
+            System.out.println("Poupança com tributo: "+calculo.tributosPoupanca()+"\nCorrente com tributo:  "+(calculo.tributosContaCorrente()+calculo.getSaldoCorrente())+"\nSeguro de vida com tributo: "+calculo.tributosSeguro());
+            System.out.println("---------------------------------------------------------------------");
         }
+
 
 
     }
